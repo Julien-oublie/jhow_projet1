13a1d9530889a931bb630eb16bbcd7ea5717d17c
julien <julienoublie@gmail.com>
2022-02-06 20:10:43 +0100

    a changer

--- assets/components/Navbar.jsx
+++ assets/components/Navbar.jsx
@@ -1,12 +1,48 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import {render} from 'react-dom';
+import { useEffect } from 'react/cjs/react.development';
 
 function MenuNavigation(){
+
+
+    const [etat , setEtat] = useState('link-active')
+
+
+
+    
+
+
+    const changeEtat  = (e) =>{
+        const element = e.target
+        
+        if(element.className == etat){
+            element.classList.remove(etat)
+        }else{
+            
+            element.classList.add(etat)
+            const link = document.getElementsByClassName(etat)
+            const link0 = document.getElementsByClassName(etat)[0]
+            const link1 = document.getElementsByClassName(etat)[1]
+            console.log(link)
+            console.log(link0)
+            console.log(link1)
+            
+            if(link1 != undefined){
+                link1.classList.remove(etat)
+
+
+            }    
+            
+        }
+        
+        
+    }
+    
     return <div className='navbar'>
         <ul>
-            <li> <a href='/regles'> Règles </a></li>
-            <li> <a href='/personnage'>Personnages</a> </li>
-            <li> <a href="/fiche">Fiche</a> </li>
+            <li onClick={changeEtat }>  Règles </li>
+            <li  onClick={changeEtat}> Personnages </li>
+            <li onClick={changeEtat}>   Fiche </li>
         </ul>
     </div>
 }
