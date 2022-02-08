59d0f2b463f99d6f1ee511e4dd239b7115e812bc
Ophelie <ophelie.rochefeuille@green-conseil.com>
2022-02-08 18:01:21 +0100

    Added ring circle

--- assets/app.js
+++ assets/app.js
@@ -8,7 +8,9 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
 
-import './components/Navbar.jsx'
+import './components/Navbar.jsx';
+import './components/Register.jsx';
+import './components/Inscrition';
 
 // start the Stimulus application
 import './bootstrap';
