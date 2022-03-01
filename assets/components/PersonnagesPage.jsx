0ae015ccf49028c76d5adc1589ee6c1dc68c9fe6
Ophelie <ophelie.rochefeuille@green-conseil.com>
2022-03-01 16:33:31 +0100

    Added fake data JSON

--- assets/components/PersonnagesPage.jsx
+++ assets/components/PersonnagesPage.jsx
@@ -19,19 +19,36 @@ function PersonnagesPage(){
         "caractéristiques_2_title": "Barding",
         "caractéristiques_2_content": "Sed sed convallis dui, eu consectetur enim. Quisque tincidunt nunc nec faucibus mattis.",
         "caractéristiques_3_title": "Barding",
+        "caractéristiques_3_content": "Sed sed convallis dui, eu consectetur enim. Quisque tincidunt nunc nec faucibus mattis."},
+        {class:"Homme", 
+        "description": "Sed sed convallis dui, eu consectetur enim. Quisque tincidunt nunc nec faucibus mattis. Quisque semper nisl nec ipsum rutrum dictum. Etiam gravida, massa quis vehicula iaculis, enim risus maximus nisl, at interdum nibh arcu non ante. Curabitur ultricies leo et purus convallis pellentesque in ut erat.",
+        "caractéristiques_1_title": "Barding",
+        "caractéristiques_1_content": "Sed sed convallis dui, eu consectetur enim. Quisque tincidunt nunc nec faucibus mattis.",
+        "caractéristiques_2_title": "Barding",
+        "caractéristiques_2_content": "Sed sed convallis dui, eu consectetur enim. Quisque tincidunt nunc nec faucibus mattis.",
+        "caractéristiques_3_title": "Barding",
         "caractéristiques_3_content": "Sed sed convallis dui, eu consectetur enim. Quisque tincidunt nunc nec faucibus mattis."}]
 
     return <div className='main-div-personnages'>
        <div className='background-personnages'></div>
 
-       <div className='div-navigation-slide-left'></div>
-       <span class="swipe-div">Swipe de classes</span>
+        <div className='swipe-main-div'>
+        <div className='div-navigation-slide-left'></div>
+       <span className="swipe-div">Swipe de classes</span>
        <div className='div-navigation-slide-right'></div>
+       <p>{data[0].class}</p>
+       <p>{data[1].class}</p>
+       <button onClick={swipe}>Swipe</button>
+        </div>
        
-       {data.map(person => <div key={person.class}>{`${person.class} ${person.description}`}</div>)}
     </div>
 }
 
+function swipe(){
+    for (let i= 0; i< data.length; i++){
+        console.log(i)
+    }
+}
 class Personnages extends HTMLElement {
     connectedCallback(){
         render(<PersonnagesPage/>,this)
