9c13bc5c16e02ed7f5283e2ff0a81c1cad554c40
Wasef <alex.wasef@gmail.com>
2022-04-07 11:29:14 +0200

    pdf

--- migrations/Version20220406104420.php
+++ migrations/Version20220406104420.php
@@ -22,13 +22,11 @@ final class Version20220406104420 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE attribut_ameliores (id INT AUTO_INCREMENT NOT NULL, personnage_id INT DEFAULT NULL, corps INT NOT NULL, coeur INT NOT NULL, esprit INT NOT NULL, UNIQUE INDEX UNIQ_FC40A4125E315342 (personnage_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE attribut_ameliores ADD CONSTRAINT FK_FC40A4125E315342 FOREIGN KEY (personnage_id) REFERENCES personnage (id)');
-        $this->addSql('ALTER TABLE user DROP pseudo');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE attribut_ameliores');
-        $this->addSql('ALTER TABLE user ADD pseudo VARCHAR(255) NOT NULL');
+       
     }
 }
